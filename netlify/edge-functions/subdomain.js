ab6841f0f2546dd0e77bd69c3a435df240b56931
Chris Hannaby <chris.hannaby@netlify.com>
2023-01-02 18:56:24 -0700

    apply first experiment using HTML rewriter

--- netlify/edge-functions/subdomain.js
+++ netlify/edge-functions/subdomain.js
@@ -1,4 +1,5 @@
-import { connect } from "https://unpkg.com/@planetscale/database";
+import { connect } from "https://unpkg.com/@planetscale/database@1.5.0/dist/index.js";
+import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
 
 const config = {
   host: Deno.env.get("DATABASE_HOST"),
@@ -6,12 +7,19 @@ const config = {
   password: Deno.env.get("DATABASE_PASSWORD"),
 };
 
-async function getSiteFromSubdomain(subdomain) {
-  const conn = await connect(config);
-  const sitesData = await conn.execute("SELECT * FROM sites WHERE name = ?", [
+async function getSiteFromSubdomain(db, subdomain) {
+  const sitesData = await db.execute("SELECT * FROM sites WHERE name = ?", [
     subdomain,
   ]);
   return sitesData.rows[0];
+}
+
+async function getExperimentsForSite(db, siteId) {
+  const experimentsData = await db.execute(
+    "SELECT * FROM experiments WHERE site_id = ?",
+    [siteId]
+  );
+  return experimentsData.rows;
 }
 
 export default async function handler(req, context) {
@@ -21,11 +29,30 @@ export default async function handler(req, context) {
   if (subdomain === "www" || subdomain === "localhost:8888") {
     return;
   }
-  const site = await getSiteFromSubdomain(subdomain);
+  const db = await connect(config);
+  const site = await getSiteFromSubdomain(db, subdomain);
   if (!site) {
     return context.rewrite("/404.html");
   } else {
     const url = site.url + reqUrl.pathname;
-    return await fetch(url);
+    const response = await fetch(url);
+    if (response.headers.get("content-type").includes("text/html")) {
+      const experiements = await getExperimentsForSite(db, site.id);
+      if (experiements.length > 0) {
+        const experiment = experiements[0];
+        const transformedResponse = new HTMLRewriter()
+          .on(experiment.selector, {
+            element(element) {
+              element.setInnerContent(experiment.replacement_text);
+            },
+          })
+          .transform(response);
+        return transformedResponse;
+      } else {
+        return response;
+      }
+    } else {
+      return response;
+    }
   }
 }
