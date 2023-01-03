76f2cebcd94955e5a1278a7792724df753a17295
Chris Hannaby <chris.hannaby@netlify.com>
2023-01-02 17:11:19 -0700

    return the site for www

--- netlify/edge-functions/subdomain.js
+++ netlify/edge-functions/subdomain.js
@@ -1,4 +1,10 @@
 export default async function handler(req, context) {
+  const host = new URL(req.url).host;
+  const subdomain = host.split(".")[0];
+  if (subdomain === "www") {
+    return;
+  }
+
   return new Response(`Requested from: ${req.url}`, {
     status: 200,
   });
