2971cc253f65d24978c11daccdc7e9a8ee6c3960
Chris Hannaby <chris.hannaby@netlify.com>
2023-01-02 17:50:38 -0700

    fix planetscale query

--- netlify/edge-functions/subdomain.js
+++ netlify/edge-functions/subdomain.js
@@ -1,30 +1,31 @@
 import { connect } from "https://unpkg.com/@planetscale/database";
 
 const config = {
-  host: Deno.env.get("PS_HOST"),
-  username: Deno.env.get("PS_USERNAME"),
-  password: Deno.env.get("PS_PASSWORD"),
+  host: Deno.env.get("DATABASE_HOST"),
+  username: Deno.env.get("DATABASE_USERNAME"),
+  password: Deno.env.get("DATABASE_PASSWORD"),
 };
 
 async function getSiteFromSubdomain(subdomain) {
   const conn = await connect(config);
-  const sitesData = await conn.excecute(
-    `SELECT * FROM sites WHERE subdomain = ?`,
-    subdomain
-  );
+  const sitesData = await conn.execute("SELECT * FROM sites WHERE name = ?", [
+    subdomain,
+  ]);
   return sitesData.rows[0];
 }
 
 export default async function handler(req, context) {
   const host = new URL(req.url).host;
   const subdomain = host.split(".")[0];
-  if (subdomain === "www") {
+  if (subdomain === "www" || subdomain === "localhost:8888") {
     return;
   }
 
+  console.log("subdomain", subdomain);
   const site = await getSiteFromSubdomain(subdomain);
+  console.log("site", site);
   if (!site) {
-    context.rewrite = "/404.html";
+    return context.rewrite("/404.html");
   } else {
     return context.json(site);
   }
