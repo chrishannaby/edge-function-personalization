c4f507a48bb6a56d839b4febe28da0847e96e7b3
Chris Hannaby <chris.hannaby@netlify.com>
2023-01-02 17:39:56 -0700

    read data from planetscale

--- netlify/edge-functions/subdomain.js
+++ netlify/edge-functions/subdomain.js
@@ -1,3 +1,20 @@
+import { connect } from "https://unpkg.com/@planetscale/database";
+
+const config = {
+  host: Deno.env.get("PS_HOST"),
+  username: Deno.env.get("PS_USERNAME"),
+  password: Deno.env.get("PS_PASSWORD"),
+};
+
+async function getSiteFromSubdomain(subdomain) {
+  const conn = await connect(config);
+  const sitesData = await conn.excecute(
+    `SELECT * FROM sites WHERE subdomain = ?`,
+    subdomain
+  );
+  return sitesData.rows[0];
+}
+
 export default async function handler(req, context) {
   const host = new URL(req.url).host;
   const subdomain = host.split(".")[0];
@@ -5,7 +22,10 @@ export default async function handler(req, context) {
     return;
   }
 
-  return new Response(`Requested from: ${req.url}`, {
-    status: 200,
-  });
+  const site = await getSiteFromSubdomain(subdomain);
+  if (!site) {
+    context.rewrite = "/404.html";
+  } else {
+    return context.json(site);
+  }
 }
